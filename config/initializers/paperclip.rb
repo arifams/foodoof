893f0e187eb6f5bbdbd67864e3356f8c3c4d5f15
arifams <arif.amsterdam@gmail.com>
2016-11-15 11:37:06 +0100

    add s3 foodoof host name to us west 1

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,4 @@
 # needed for paperclip international user
 Paperclip::Attachment.default_options[:url] = ':foodoof.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
